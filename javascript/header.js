9d06443091746499653a8b5ac7a44399da600404
Luke Hayes <lukehayes2021@u.northwestern.edu>
2019-08-06 18:45:36 -0500

    Demo Day

--- javascript/header.js
+++ javascript/header.js
@@ -1,26 +1,26 @@
-$(document).ready(function() {
-  $(".title").lettering();
-  $(".button").lettering();
-});
+// $(document).ready(function() {
+//   $(".title").lettering();
+//   $(".button").lettering();
+// });
 
 
 
 
 
-$(document).ready(function() {
-  animation();
-}, 1000);
+// $(document).ready(function() {
+//   animation();
+// }, 1000);
 
-$('.button').click(function() {
-  animation();
-});
+// $('.button').click(function() {
+//   animation();
+// });
 
 
-function animation() {
-  var title1 = new TimelineMax();
-  title1.to(".button", 0, {visibility: 'hidden', opacity: 0})
-  title1.staggerFromTo(".title span", 0.7, 
-  {ease: Back.easeOut.config(1.7), opacity: 0, bottom: -90},
-  {ease: Back.easeOut.config(1.7), opacity: 1, bottom: 0}, 0.6);
-  title1.to(".button", 0.2, {visibility: 'visible' ,opacity: 1})
-}
+// function animation() {
+//   var title1 = new TimelineMax();
+//   title1.to(".button", 0, {visibility: 'hidden', opacity: 0})
+//   title1.staggerFromTo(".title span", 0.7, 
+//   {ease: Back.easeOut.config(1.7), opacity: 0, bottom: -90},
+//   {ease: Back.easeOut.config(1.7), opacity: 1, bottom: 30}, 0.6);
+//   title1.to(".button", 0.2, {visibility: 'visible' ,opacity: 1})
+// }
