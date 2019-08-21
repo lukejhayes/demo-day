0cb23300f59f0d0f54539887378b379b2ca16451
Luke Hayes <lukehayes2021@u.northwestern.edu>
2019-08-21 12:28:32 -0500

    More React

---
+++ public/login/build/precache-manifest.4cb99747af18658ccb6fb75dd12cb739.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89901a988bd3a59198f12439cca41485",
+    "url": "/index.html"
+  },
+  {
+    "revision": "680089bb5ea89bc115fe",
+    "url": "/static/css/main.d20fb327.chunk.css"
+  },
+  {
+    "revision": "4ae0499fb66eaa33d311",
+    "url": "/static/js/2.867e9d40.chunk.js"
+  },
+  {
+    "revision": "680089bb5ea89bc115fe",
+    "url": "/static/js/main.2268f799.chunk.js"
+  },
+  {
+    "revision": "e9b31832738984fc7561",
+    "url": "/static/js/runtime~main.5c42d372.js"
+  }
+]);
