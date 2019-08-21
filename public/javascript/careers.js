da719cd4bb3d06aef0e05f3fe7e872cfa074d9f9
Joey Lansangan <joeylansangan@gmail.com>
2019-08-20 22:25:05 -0500

    new careers

--- public/javascript/careers.js
+++ public/javascript/careers.js
@@ -1,7 +1,26 @@
 document.addEventListener('DOMContentLoaded',function(event){
+  var modal = document.getElementById("myModal");
+  // Get the button that opens the modal
+  var btn = document.getElementById("myBtn");
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("close")[0];
+  btn.onclick = function() {
+    modal.style.display = "block";
+  }
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() {
+  modal.style.display = "none";
+}
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+
     // array with texts to type in typewriter
     var dataText = [ "Join the team . . .", "Share the experience."];
-    
     // type one text in the typwriter
     // keeps calling itself until the text is finished
     function typeWriter(text, i, fnCallback) {
